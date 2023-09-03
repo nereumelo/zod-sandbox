5ef73bcf2d29f25ab474f29d7ac4dda3dad2db62
nereumelo <nereuvictor1@gmail.com>
2023-09-03 11:58:58 -0300

    Add a card to error page and improve styling

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom'
+import { Card } from 'flowbite-react'
+import { H2 } from '../components/headers/H2'
 
 export function ErrorPage() {
   return (
-    <div>
-      <h3>Error 404!</h3>
-      <p>Return to <Link to="/">Home</Link></p>
+    <div className='h-[76.8vh] w-full flex justify-center items-center'>
+      <Card className='h-52 w-80 flex-row justify-center items-center'>
+        {/* <img src = "undraw_empty.svg" alt="My Happy SVG" className='h-full'/> */}
+        {/* <div> */}
+          <H2>Page not found</H2>
+          <p className='text-base'>
+            Return to <Link to="/" className='text-blue-500 font-semibold underline hover:text-blue-800'>Home</Link>
+          </p>
+        {/* </div> */}
+      </Card>
     </div>
   )
 }
