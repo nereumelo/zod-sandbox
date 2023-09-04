4d54e566b42e46543ca8e5d89664e96b3f7f58e8
nereumelo <nereuvictor1@gmail.com>
2023-09-04 01:31:10 -0300

    Improve layout of error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,19 +1,16 @@
 import { Link } from 'react-router-dom'
-import { Card } from 'flowbite-react'
 import { H2 } from '../components/headers/H2'
 
 export function ErrorPage() {
   return (
-    <div className='h-[76.8vh] w-full flex justify-center items-center'>
-      <Card className='h-52 w-80 flex-row justify-center items-center'>
-        {/* <img src = "undraw_empty.svg" alt="My Happy SVG" className='h-full'/> */}
-        {/* <div> */}
-          <H2 className='dark:text-white'>Page not found</H2>
-          <p className='text-base text-center dark:text-white'>
-            Return to <Link to="/" className='text-blue-500 font-semibold underline hover:text-blue-800'>Home</Link>
+    <div className='h-full w-[700px] self-center justify-self-center grid grid-cols-2 items-center justify-items-center'>
+        <img src = "undraw_empty.svg" alt="Not found" className='drop-shadow-2xl'/>
+        <div>
+          <H2 className='text-3xl font-semibold'>Page not found</H2>
+          <p className='text-lg text-center'>
+            Return to <Link to="/" className='text-blue-500 font-semibold underline hover:text-blue-600'>Home</Link>
           </p>
-        {/* </div> */}
-      </Card>
+        </div>
     </div>
   )
 }
