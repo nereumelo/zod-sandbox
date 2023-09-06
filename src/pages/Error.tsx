a5c3c23072720eaf0f7bfd1779cb6302260c7ad4
nereumelo <nereuvictor1@gmail.com>
2023-09-05 21:44:18 -0300

    Fix responsive layout of error page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -3,14 +3,16 @@ import { H2 } from '../components/headers/H2'
 
 export function ErrorPage() {
   return (
-    <div className='h-full w-[700px] self-center justify-self-center grid grid-cols-2 items-center justify-items-center'>
-        <img src = "undraw_empty.svg" alt="Not found" className='drop-shadow-2xl'/>
+    <div className='h-full w-full flex items-stretch justify-center overflow-auto'>
+      <div className='flex md:flex-row flex-col items-center justify-center gap-6'>
+        <img src = "undraw_empty.svg" alt="Not found" className='drop-shadow-2xl md:h-5/6 h-4/6 px-4'/>
         <div>
           <H2 className='text-3xl font-semibold'>Page not found</H2>
           <p className='text-lg text-center'>
             Return to <Link to="/" className='text-blue-500 font-semibold underline hover:text-blue-600'>Home</Link>
           </p>
         </div>
+      </div>
     </div>
   )
 }
